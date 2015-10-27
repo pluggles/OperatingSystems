29f7df0c26f0d691c2d3e78469f0bee6c8ede513
Aron Lawrence <aron.r.lawrence@wmich.edu>
2015-10-27 15:50:49 -0400

    stopping point

--- A3/P2A3.c
+++ A3/P2A3.c
@@ -8,26 +8,50 @@
 
 /*Threading problem */
 #define _POSIX_C_SOURCE 199309L
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <time.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/syscall.h>
+#include <string.h>
+#include <unistd.h>
+#include <math.h>
+
 
 
 int getArraySize (int, char **);
+void * populateArray(void *);
 
 int ARRAYSIZE;
+int *array;
+
+/*number of processes */
+#define P 4
 
 int main(int argc, char *argv[]) {
 	struct timespec start, finish;
 	double elapsed;
 	clock_gettime(CLOCK_MONOTONIC, & start);
-	
+	int i;
 	if (getArraySize(argc, argv) == -1) { /*get the array size from command line */
 		return -1;
 	}
+	array = malloc (sizeof(int) * ARRAYSIZE);
+	pthread_t threads[P];
 
-
+	//int array[ARRAYSIZE];
+	
+	for(i = 0; i < P; ++i) {
+		int *arg = malloc(sizeof(*arg));
+		*arg = i;
+		if(pthread_create(&threads[i], NULL, populateArray, arg)) {
+			perror("pthread create: ");
+			exit(1);
+		}
+	}
 
 	clock_gettime(CLOCK_MONOTONIC, &finish);
 	elapsed = (finish.tv_sec - start.tv_sec);
@@ -55,4 +79,41 @@ int getArraySize (int argc, char *argv[]) {
 		return 0;
 	}
 
+}
+
+void * populateArray(void *x) {
+	int thread = *((int*) x);
+	free(x);
+	double base = floor(ARRAYSIZE/4);
+	double start, end; //index
+	switch (thread) {
+		case 0:
+			printf("thread value is: %d\n", thread);
+			start = 0;
+			end = floor(ARRAYSIZE/4);
+			break;
+		case 1:
+			start = floor(ARRAYSIZE/4) + 1;
+			end = floor(ARRAYSIZE/2);
+			printf("thread value is: %d\n", thread);
+			break;
+		case 2:
+			start = floor(ARRAYSIZE/2) + 1;
+			end = ARRAYSIZE*3
+			printf("thread value is: %d\n", thread);
+			break;
+		case 3:
+			start = ARRAYSIZE*3 + 1;
+			end = ARRAYSIZE-1;
+			printf("thread value is: %d\n", thread);
+			break;
+		default:
+			printf("something is wrong\n");
+
+		for(start; start <=end; start++){
+			
+		}
+	}
+
+	pthread_exit(0);
 }
