d02744980542e5bcfb867ab715a742d235f6ca3b
Aron Lawrence <aron.r.lawrence@wmich.edu>
2015-11-20 17:28:06 -0500

    adding the a4 dir

---
+++ A4/ProjectA4.c
@@ -0,0 +1,28 @@
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+
+struct student {
+int studentNumber;
+int courseCredit;
+float grade;
+struct list_head list;
+};
+/* This function is called when the module is loaded. */
+int simple_init(void)
+{
+printk(KERN_INFO "Hello World\n");
+}
+
+/* This function is called when the module is loaded. */
+int simple_exit(void)
+{
+printk(KERN_INFO "Goodbye World\n");
+}
+/* Macros for registering module entry and exit points. */
+module_init(simple_init);
+module_exit(simple_exit);
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Simple Module");
+MODULE_AUTHOR("Aron Lawrence");
